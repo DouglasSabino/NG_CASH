b55ad85369c71c1a8a05bd2c049fdac847253f3a
Douglas Sabino <douglasbsabino@hotmail.com>
2022-12-10 22:29:58 -0300

    faz validação simples de senha

--- src/models/modelsLogin.js
+++ src/models/modelsLogin.js
@@ -1,14 +1,25 @@
+const bcrypy = require('bcrypt');
 const { db } = require('./connection');
 
 const modelsLogin = {
   verifyIfUserExist: async (username) => {
     const SQL_GET_USER = 'SELECT * FROM Users WHERE username=?';
     const [user] = await db.query(SQL_GET_USER, [username]);
+    console.log(user);
     return user;
   },
+  verifyPassword: async (password, salt, hash) => {
+    if (bcrypy.hashSync(password, salt) === hash) return true;
+    return false; 
+  },
   login: async (body) => {
-    const { username } = body;
+    const { username, password } = body;
     const user = await modelsLogin.verifyIfUserExist(username);
+    if (user.length !== 0) {
+      const auth = await modelsLogin.verifyPassword(password,user[0].salt, user[0].password);
+      if(auth) console.log('Autorizado');
+      else console.log('Não Autorizado');
+    }
     return user;    
   },
 };
