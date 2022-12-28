1663358b43b6ae52334b96e6f912a58162cb5f9f
Douglas Sabino <douglasbsabino@hotmail.com>
2022-12-27 23:11:26 -0300

    implementa logica parar exibir e esconder saldo da conta

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext, useRef } from 'react';
 import appContext from '../context/appContext';
 import { AiFillEye } from "react-icons/ai";
 import Axios from 'axios';
 
 function Home() {
+  const containerRef = useRef();
   const { balance, setBalance, user, setUser } = useContext(appContext);
   useEffect(() => {
    const user = JSON.parse(localStorage.getItem('user'));
@@ -17,14 +18,38 @@ function Home() {
    }).then((result) => {
       setBalance(result.data[0].balance);
    })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   },[]);
+
+  const changePassword = () => {
+    let situation = containerRef.current.className;
+    const vetorSituation = situation.split(' ');
+    if (situation.includes('enable')) {
+      const balanceValue = containerRef.current.innerText;
+      const vetorBalance = balanceValue.split('');
+      const cryptBalanceVetor = vetorBalance.map((_e) => '*');
+      const cryptBalance = cryptBalanceVetor.join('');
+      containerRef.current.innerText = cryptBalance;
+      vetorSituation.pop();
+      const s = vetorSituation.join(',');
+      const replace = s.replace(/,/g, " ");
+      containerRef.current.className = replace
+    }else {
+      containerRef.current.innerText = balance;
+      vetorSituation.push('enable');
+      const s = vetorSituation.join(',');
+      const replace = s.replace(/,/g, " ");
+      containerRef.current.className = replace
+    }
+  }
 
   return (
    <div className='h-[100vh] w-[100vw]'>
     <div className='pl-16 bg-slate-400 shadow-md flex justify-center' >
      <h1 className='font-gentium text-4xl mr-auto' >Olá {user.username}</h1>
-     <h2 className='font-gentium text-4xl flex pr-5'>{`Saldo Atual: ${balance}`}</h2>
-     <div  className='text-4xl pr-16  pt-1'>
+     {/*Deixar class 'enable' sempre por ultimo no atributo className da tag p'*/}
+     <h2 className='font-gentium text-4xl flex pr-5'>Saldo Atual: <p className='w-20 ml-3 mr-3 enable' ref={containerRef}>{balance}</p></h2>
+     <div onClick={ changePassword } className='text-4xl pr-16  pt-1'>
       <AiFillEye />
      </div>
     </div>
